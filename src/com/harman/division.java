e841b8c88883c7bd77411cca3b0988aa0aa677d8
Nithinrab <nithinrab08@gmail.com>
2022-03-21 12:35:02 +0530

    divisible by 8

---
+++ src/com/harman/division.java
@@ -0,0 +1,12 @@
+package com.harman;
+
+
+public class division {
+    public static void main(String[] args) {
+    int a=17;
+    if(a%8==0)
+        System.out.println("Is divisible.");
+    else
+        System.out.println("Is not divisible.");
+    }
+}
