dc81ca761c4ce94f2f20bb9477ecd55a8a38f513
Nithinrab <nithinrab08@gmail.com>
2022-03-22 14:57:01 +0530

    multi

--- src/com/harman/MultiThreadExample.java
+++ src/com/harman/MultiThreadExample.java
@@ -1,19 +1,33 @@
 package com.harman;
 
-class Task extends Thread{
+class Task1 extends Thread{
     @Override
     public void run() {
-        System.out.println("thread executed -"+Thread.currentThread().getName());
+        System.out.println("task 1 thread executed -"+Thread.currentThread().getName());
+
+    }
+}
+class Task2 extends Thread {
+    @Override
+    public void run() {
+        System.out.println("task 2 thread executed -" + Thread.currentThread().getName());
+
+    }
+}
+class Task3 extends Thread {
+    @Override
+    public void run() {
+        System.out.println("task 3 thread executed -" + Thread.currentThread().getName());
 
     }
 }
 public class MultiThreadExample{
     public static void main(String[] args) {
-        Task t = new Task();
-        t.start();
-        Task t1 = new Task();
-        t1.start();
-        Task t2=new Task();
+       Task1 t1 = new Task1();
+       t1.start();
+        Task2 t2 = new Task2();
         t2.start();
+        Task3 t3 = new Task3();
+        t3.start();
     }
 }
